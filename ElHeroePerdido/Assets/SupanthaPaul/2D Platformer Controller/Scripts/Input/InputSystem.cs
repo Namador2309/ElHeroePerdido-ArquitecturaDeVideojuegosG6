841cf31a19070e239c9d80fdc1ec51867c57b760
Jeancr14 <jsegura30896@ufide.ac.cr>
2026-03-08 23:49:22 -0600

    Initial Unity project - nivel bosque con cascada
    Primer avance del videojuego con personaje, escenario de bosque y cascada.

---
+++ ElHeroePerdido/Assets/SupanthaPaul/2D Platformer Controller/Scripts/Input/InputSystem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace SupanthaPaul
+{
+    public class InputSystem : MonoBehaviour
+    {
+        public static float HorizontalRaw()
+        {
+            float h = 0f;
+
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                h = -1f;
+
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                h = 1f;
+
+            return h;
+        }
+
+        public static bool Jump()
+        {
+            return Input.GetKeyDown(KeyCode.Space);
+        }
+
+        public static bool Dash()
+        {
+            return Input.GetKeyDown(KeyCode.X);
+        }
+    }
+}
